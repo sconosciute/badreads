6be731b18ff82c8afe0057fa09ca7c422f43ffac
Zac <andersenzb@gmail.com>
2024-05-29 12:25:30 -0700

    call .json() on the fetch() response to get books

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -5,18 +5,21 @@ import Box from "@mui/material/Box";
 import BookCard from "@/components/BookCard";
 
 async function getAllBooks(page: number = 1, pageSize: number = 10) {
-    const res = await fetch(`${baseUrl}/books/all?page=${page}&pageSize=${pageSize}`) as IBookResponse
-    console.dir(res);
-    return res.entries;
+    const res = await fetch(`${baseUrl}/books/all?page=${page}&pageSize=${pageSize}`);
+    const resJ = await res.json();
+    console.dir(resJ);
+    return resJ.entries;
 }
 
 export default async function SearchResults({query} : {query: string}) {
 
+    console.log("grabbing books");
     const books = await getAllBooks();
+    console.dir(books);
 
     return (
         <Box sx={{padding: "1vw", flexWrap: 'wrap'}} display="flex" alignItems="center">
-            {books.map((book) => (
+            {books.map((book: IBook) => (
                 <BookCard book={book} />
             ))}
         </Box>

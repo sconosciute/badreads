6bf5599f92e63965718e3f421a4bcf92ed63b3f6
enriquev5 <enriquevargas972@gmail.com>
2024-05-28 19:25:49 -0700

    WIP made the card expand so the full book details can be seen (still need to add) added update book button (non functional)

--- src/app/bookDetails/page.tsx
+++ src/app/bookDetails/page.tsx
@@ -1,44 +1,44 @@
+"use client"
+
 import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import BookCard from "@/components/BookCard";  // Ensure this component is imported correctly
 import Button from "@mui/material/Button";
-import NextLink from "next/link";
-import Link from "@mui/material/Link";
-import Rating from "@mui/material/Rating";
-import {width} from "@mui/system";
 
-export default function bookDetails() {
+export default function BookDetails() {
+    const [isExpanded, setIsExpanded] = React.useState(false);
+
+    const handleBookCardClick = () => {
+        setIsExpanded(!isExpanded);
+    };
+
     return (
-        <Container maxWidth="lg" sx={{display:'flex', flexFlow: "row"}}>
+        <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column', justifyContent: "center", alignItems: "center", height: "80vh" }}>
 
-            <Link href = "https://www.washington.edu/" underline={"hover"}>
-                <Box sx={{ minWidth: 600, borderRadius: "5px",display: "flex" , backgroundColor: '#E0DFD5', flex: '0 0 60%'}}>
-                    <Box
-                        margin={"1em"}
-                        component="img"
-                        sx={{
-                            height: 400,
-                            width: 270,
-                            maxHeight: { xs: 800, md: 1000 },
-                            maxWidth: { xs: 540, md: 675 },
-                        }}
-                        alt="Hunger Games 1 cover"
-                        src="https://images.gr-assets.com/books/1447303603s/2767052.jpg"
-                    />
+                <Box sx={{ minWidth: 200, borderRadius: "5px", display: "flex", flexDirection: "column", backgroundColor: '#CBAC8F', marginBottom: 4 }}
+                onClick={handleBookCardClick}
+            >
+                <BookCard
+                    book={{
+                        title: "Title",
+                        authors: "Author",
+                        ratings: { average: 4.5 },
+                        icons: { large: "https://images.gr-assets.com/books/1447303603s/2767052.jpg" }
+                    }}
+                />
 
-                    <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', color:'#313638'}}>
-                        <Typography variant="h4" component="div">
-                            Title
-                        </Typography>
-                        <Typography component="div">
-                            Author
-                        </Typography>
-                        <Rating precision={0.1} name="read-only" value={4.5} readOnly />
+                {isExpanded && (
+                    <Box sx={{ padding: 1 }}>
+                        <Typography variant="h6" align= 'center'>Full Book Details</Typography>
                     </Box>
-                </Box>
-            </Link>
-            <Box sx = {{width: 50 , display: "flex", flexDirection: "column"}}>heeey</Box>
+                )}
+
+                <Button variant="contained" sx={{ backgroundColor: '#F06543', height: '50px', width: '150px', mt: 1, alignSelf: 'center' }}>
+                    Update Book
+                </Button>
+            </Box>
         </Container>
     );
 }

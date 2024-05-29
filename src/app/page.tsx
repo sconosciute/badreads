fbaa98628fd76730c57dcf63f5da428664e3c835
Zac <andersenzb@gmail.com>
2024-05-28 17:32:33 -0700

    Cleaned out unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import NextLink from "next/link";
-import ProTip from "@/components/ProTips";
-import Informational from "@/components/Informational";
-import BookCard from "@/components/BookCard";
 import {testBook} from "@/Common";
 import * as test from "node:test";
 import SearchResults from "@/components/SearchResults";

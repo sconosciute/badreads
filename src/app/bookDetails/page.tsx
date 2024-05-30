34096c6990b2bd7dd3c50af498bde934599fcf1f
enriquev5 <enriquevargas972@gmail.com>
2024-05-29 21:05:44 -0700

    removed old layout and added the bookpreview alt branch layout into here

--- src/app/bookDetails/page.tsx
+++ src/app/bookDetails/page.tsx
@@ -1,43 +1,75 @@
-"use client"
-
 import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import BookCard from "@/components/BookCard";  // Ensure this component is imported correctly
 import Button from "@mui/material/Button";
+import NextLink from "next/link";
+import Link from "@mui/material/Link";
+import Rating from "@mui/material/Rating";
+import {width} from "@mui/system";
 
-export default function BookDetails() {
-    const [isExpanded, setIsExpanded] = React.useState(false);
 
-    const handleBookCardClick = () => {
-        setIsExpanded(!isExpanded);
-    };
-
+export default function bookDetails() {
     return (
-        <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column', justifyContent: "center", alignItems: "center", height: "80vh" }}>
-
-                <Box sx={{ minWidth: 200, borderRadius: "5px", display: "flex", flexDirection: "column", backgroundColor: '#CBAC8F', marginBottom: 4 }}
-                onClick={handleBookCardClick}
-            >
-                <BookCard
-                    book={{
-                        title: "Title",
-                        authors: "Author",
-                        ratings: { average: 4.5 },
-                        icons: { large: "https://images.gr-assets.com/books/1447303603s/2767052.jpg" }
+        <Container maxWidth="lg" sx={{display:'flex', flexFlow: "row", marginTop: "1em", gap: "2em"}}>
+            <Box sx={{ minWidth: 600, borderRadius: "5px",display: "flex" , backgroundColor: '#E0DFD5', flex: '0 0 70%'}}>
+                {/*Book image*/}
+                <Box
+                    component="img"
+                    margin={"1em"}
+                    sx={{
+                        height: 400,
+                        width: 270,
+                        maxHeight: { xs: 800, md: 1000 },
+                        maxWidth: { xs: 540, md: 675 },
                     }}
+                    alt="Hunger Games 1 cover"
+                    src="https://images.gr-assets.com/books/1447303603s/2767052.jpg"
                 />
 
-                {isExpanded && (
-                    <Box sx={{ padding: 1 }}>
-                        <Typography variant="h6" align= 'center'>Full Book Details</Typography>
-                    </Box>
-                )}
 
-                <Button variant="contained" sx={{ backgroundColor: '#F06543', height: '50px', width: '150px', mt: 1, alignSelf: 'center' }}>
-                    Update Book
-                </Button>
+                {/*Main information*/}
+                <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', color:'#313638'}}>
+                    <Typography variant="h3">
+                        Title
+                    </Typography>
+                    <Typography sx={{fontSize: "1.8em"}}>
+                        Author
+                    </Typography>
+                    <Typography>
+                        ISBN-13: [isbn]
+                    </Typography>
+
+
+                    <Box>
+                        <Box>
+                            <Rating precision={0.1} size={"large"} value={4.5} readOnly sx={{marginTop:"1em"}} />
+                            <Typography >
+                                Average rating: [4.5] stars
+                            </Typography>
+                        </Box>
+
+                        <Typography sx={{marginTop: "1em"}}>
+                            One star ratings: [number] <br/>
+                            Two star ratings: [number] <br/>
+                            Three star ratings: [number] <br/>
+                            Four star ratings: [number] <br/>
+                            Five star ratings: [number] <br/>
+                        </Typography>
+                    </Box>
+
+
+                    <Box sx={{justifyContent:'space-around'}}>
+                        <Button variant="contained" sx={{margin:2}}>Update</Button>
+                        <Button variant="contained" sx={{margin:2}}>Delete</Button>
+                    </Box>
+                </Box>
+            </Box>
+
+
+            {/*Recommendation sidebar*/}
+            <Box sx = {{width: 50 , display: "flex", flexDirection: "column", flex: '0 0 30%'}}>
+                (Other books would go here)
             </Box>
         </Container>
     );
